b54c0463af24fdd65192be08c82f23e2e5054a30
vrishank-03 <100521118+vrishank-03@users.noreply.github.com>
2025-09-01 18:26:52 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,35 +1,3 @@
-// import axios from 'axios';
-
-// const API_URL = 'http://localhost:5000/api';
-
-// export const uploadDocument = async (file) => {
-//   const formData = new FormData();
-//   formData.append('document', file); // 'document' must match the key in multer
-
-//   try {
-//     const response = await axios.post(`${API_URL}/documents/upload`, formData, {
-//       headers: {
-//         'Content-Type': 'multipart/form-data',
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error uploading document:", error);
-//     throw error.response ? error.response.data : new Error("Network Error");
-//   }
-// };
-
-// export const search = async (query, history) => {
-//   try {
-//     // Send both the query and the history in the request body
-//     const response = await axios.post(`${API_URL}/search`, { query, history });
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error performing search:", error);
-//     throw error.response ? error.response.data : new Error("Network Error");
-//   }
-// };
-
 import axios from 'axios';
 const API_URL = 'http://localhost:5000/api';
 
